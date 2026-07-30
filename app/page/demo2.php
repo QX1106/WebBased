525b98dc1546bbed49eb4afc5d634bb053c22f0d
LAPTOP-F47MPO11\Admin <nicoletan1044@gmail.com>
2026-07-30 22:30:20 +0800

    commit for practical 2 code

---
+++ app/page/demo2.php
@@ -0,0 +1,48 @@
+<?php
+require '../_base.php';
+// ----------------------------------------------------------------------------
+
+// Associative array
+$states = [
+    'PJY' => 'Putrajaya',
+    'KTN' => 'Kelantan',
+    'PRK' => 'Perak',
+    'NSN' => 'Negeri Sembilan',
+    'PLS' => 'Perlis',
+    'PNG' => 'Pulau Pinang',
+    'TRG' => 'Terengganu',
+    'LBN' => 'Labuan',
+    'SBH' => 'Sabah',
+    'PHG' => 'Pahang',
+    'SRW' => 'Sarawak',
+    'MLK' => 'Melaka',
+    'KDH' => 'Kedah',
+    'SEL' => 'Selangor',
+    'JHR' => 'Johor',
+];
+
+// TODO
+$states['KUL'] = 'Kuala Lumpur';
+asort($states);
+// ----------------------------------------------------------------------------
+$_title = 'Page | Demo 2 | Ordered List';
+include '../_head.php';
+?>
+
+<!-- TODO -->
+$states['KUL'] = 'Kuala Lumpur';
+asort($states);
+
+
+<p><?= count($states) ?> state(s)</p>
+
+<ol>
+    <?php
+    foreach ($states as $k => $v) {
+        echo "<li>$k - $v</li>";
+    }
+    ?>
+</ol>
+
+<?php
+include '../_foot.php';
